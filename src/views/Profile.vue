a5d4631380ec27774106896113db754fb468010c
Eric K <erickirberger@gmail.com>
2023-08-01 18:04:07 -0400

    simple profile styling

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="profile-container">
-    <h1>Profile Page</h1>
-    <h3>Display Name: {{ userData.displayName }}</h3>
-    <button class="button is-success is-large" @click="showModal = true">
-      Change Display Name
-    </button>
-    <p>Email: {{ userData.email }}</p>
-    <p>Current City: {{ capitalizeCity(currentUser.city) }}</p>
-    <p>Registered On: {{ formattedRegistrationDate }}</p>
+    <h1 class="profile-title">Profile Page</h1>
+    <div class="columns">
+      <div class="column is-one-third">
+        <img src="../assets/images/blank-profile.webp" alt="blank profile" class="profile-img">
+      </div>
+      <div class="column">
+        <h3>Display Name: <span class="display-name">{{ userData.displayName }}</span></h3>
+        <button class="change-name-button button is-success is-large" @click="showModal = true">
+          Change Display Name
+        </button>
+        <p>Email: {{ userData.email }}</p>
+        <p>Current City: {{ capitalizeCity(currentUser.city) }}</p>
+        <p>Registered On: {{ formattedRegistrationDate }}</p>
+      </div>
+    </div>
     
     <UserPosts />
 
@@ -16,7 +23,7 @@
       <div class="modal-content">
         <div class="modal-card">
           <header class="modal-card-head">
-            <p class="modal-card-title">Modal title</p>
+            <p class="modal-card-title">Change your name, <span class="modal-display-name">{{ userData.displayName }}</span> !</p>
             <button
               class="delete"
               aria-label="close"
@@ -24,7 +31,7 @@
             ></button>
           </header>
           <section class="modal-card-body">
-            This is where we'll change it.
+            This is where we'll change it. Enter a new name here.
             <input
               type="text"
               class="input is-primary"
@@ -175,21 +182,100 @@ const capitalizeCity = (city) => {
 
 <style scoped>
 @import 'bulma/css/bulma.min.css';
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap');
 
+/* BASIC STYLE */
 .profile-container {
   min-height: 100vh;
-  width: 80vw;
+  max-width: 80vw;
   background-color: blanchedalmond;
   border-radius: 10px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: 10px auto;
-  padding: 10px;
+  margin: 20px auto;
+  padding: 20px;
+  font-family: 'Roboto', sans-serif;
 }
 .line-through {
   text-decoration: line-through;
   /* make apps w. danny, min22 */
 }
+
+/* TITLE STYLE */
+.profile-title {
+  font-size: 24px; /* Increase the font size for the profile title */
+  margin-bottom: 15px; /* Add some spacing below the title */
+}
+
+.columns {
+  width: 100%;
+}
+
+/* PROFILE IMG STYLE */
+.profile-img {
+  border: 2px solid black;
+  border-radius: 10px;
+}
+
+.display-name {
+  font-size: 20px;
+  text-decoration: underline;
+}
+
+/* CHANGE NAME BUTTON STYLE */
+.change-name-button {
+  background-color: #4caf50;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.change-name-button:hover {
+  background-color: #45a049;
+}
+
+/* MODAL STYLE */
+.modal-content {
+  background-color: #fff;
+  border-radius: 5px;
+  padding: 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.modal-card-title {
+  font-size: 20px;
+  margin-bottom: 10px;
+}
+
+.modal-display-name {
+  font-size: 24px;
+  text-decoration: underline;
+}
+
+.input.is-primary {
+  width: 100%;
+  padding: 8px 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.help.is-danger {
+  color: #e74c3c;
+}
+
+/* Style the user details */
+h3, p {
+  margin-bottom: 10px;
+}
+
+/* Add some margin below the user posts section */
+#user-posts {
+  margin-top: 20px;
+}
+
 </style>
